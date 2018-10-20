b1d10ed6066dd34f2f73c8de9a82d871ed0ef28b
Onur Coskun <onurcoskun99@gmail.com>
2018-10-20 23:38:23 +0300

    functionality added to ımagelinkform component

--- face-recognition-app/src/App.js
+++ face-recognition-app/src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import Clarifai from 'clarifai';
 import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
+
+const app = new Clarifai.App({
+ apiKey: 'c2b9fd0be81843d1bbc6c067ac2fe2bd'
+});
+
 
 const ParticlesOptions= {
   particles: {
@@ -19,6 +25,33 @@ const ParticlesOptions= {
 }
 
 class App extends Component {
+
+constructor(){
+  super();
+  this.state={
+    input:'',
+  }
+}
+onInputChange=(event) =>{
+  console.log(event.target.value);
+}
+onButtonSummit=() =>{
+  console.log('click');
+app.models
+.predict(
+Clarifai.COLOR_MODEL,
+    // URL
+    "https://samples.clarifai.com/metro-north.jpg"
+)
+.then(function(response) {
+    // do something with responseconsole.log(response);
+    console.log(response);
+    },
+    function(err) {// there was an error}
+  }
+);
+}
+
   render() {
     return (
       <div className="App">
@@ -26,8 +59,12 @@ class App extends Component {
         <Navigation/>
         <Logo/>
         <Rank/>
-        <ImageLinkForm/>
+        <ImageLinkForm 
+        onInputChange={this.onInputChange}
+        onButtonSummit={this.onButtonSummit}
+        />
          {/* <FaceRecognition/>
+        }
         */}
       </div>
     );
--- face-recognition-app/src/components/ImageLinkForm/ImageLinkForm.js
+++ face-recognition-app/src/components/ImageLinkForm/ImageLinkForm.js
@@ -4,7 +4,7 @@ import Tilt from 'react-tilt';
 import './ImageLinkForm.css';
 
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({onInputChange,onButtonSummit}) => {
 	return (
 		<div>
 		<p className='f3'>
@@ -12,8 +12,10 @@ const ImageLinkForm = () => {
 		</p>
 			<div className='center'>
 				<div className='form  center pa4 br3 shadow-5'>
-				<input className='f4 pa2 w-70 center' type='text' />
-				<button className='w-30 grow f4 link ph3 pv2 dib white bg-light-blue'>Detect </button>
+				<input className='f4 pa2 w-70 center' type='text' onChange={onInputChange} />
+				<button className='w-30 grow f4 link ph3 pv2 dib white bg-light-blue'
+				onClick={onButtonSummit}
+				>Detect </button>
 				</div>
 			</div>
 		</div>
